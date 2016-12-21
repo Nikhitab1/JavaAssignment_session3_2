4d0d738403399df32a24d7e88d8c3f301ae61590
Nikhitab1 <b.nikhitha.sai@gmail.com>
2016-12-21 22:44:19 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,24 @@
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          boolean same ;
+          
+          String s1="Acad";
+          String s2="Acad";
+          if(s1==s2)   //IF both the strings are equal then print true
+          {  
+        	  
+        	  same=true;
+        	  System.out.println(same);
+        	  
+          }else           //If not same then print false 
+          {
+        	  same=false;
+        	  System.out.println(same);
+          }
+          
+	}
+
+}
